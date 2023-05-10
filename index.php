31ef2c6304a4d531a16577a32d0d4a955b9828a5
Totemokanashi <totekanjo@gmail.com>
2023-05-10 19:51:17 +0200

    aggiornato php

--- index.php
+++ index.php
@@ -4,6 +4,21 @@
     </head>
     <body>
         <table>
+            <?php
+	    echo "php generated";
+            $mysqli = new mysqli("localhost","php","");
+            $mysqli ->select_db("prodotti");
+            if ($mysqli -> connect_errno) {
+                echo "connesione fallita a mysql " . $mysqli -> connect_error;
+                exit();
+              }
+              if ($result = $mysqli -> query("SELECT * FROM prodotto")) {
+                echo "righe" . $result -> num_rows;
+                // pulisce result
+                $result -> free_result();
+              }
+              
+            ?>
             <tr></tr>
         </table>            
     </body>
