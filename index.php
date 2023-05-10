37ae1416b63d716c631df2cbd3560ec2bd6b8151
Leggio <totekanjo@gmail.com>
2023-05-10 17:36:32 +0200

    Aggiunto PHP

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Esercitazione PHP Git</title>
+    </head>
+    <body>
+        <table>
+            <tr></tr>
+        </table>            
+    </body>
+</html>
