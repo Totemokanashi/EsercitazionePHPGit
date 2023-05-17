6070b22b9c2e845f6d8e2258ebd4bf978766d9fa
Totemokanashi <totekanjo@gmail.com>
2023-05-17 23:17:19 +0200

    aggiunta barra di ricerca e immagini in locale da aggiungere sul server

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Esercitazione PHP Git</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body style="background">
-        <div class="div">
+<head>
+    <title>Esercitazione PHP Git</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background">
+    <div class="main-container">
+        <div class="search-container">
+            <form class="search-form">
+                <input type="text" name="search" placeholder="Search products...">
+                <input type="submit" value="Search">
+            </form>
+        </div>
+        <div class="card-container">
             <?php
                 $mysqli = new mysqli("localhost","php","");
                 $mysqli -> select_db("Esercitazione");
-
                 $selectProdotti = "SELECT * FROM prodotto";
-
                 if ($mysqli -> connect_errno) {
                     echo "connesione fallita a mysql " . $mysqli -> connect_error;
                     exit();
                 }
-
                 $result = $mysqli -> query($selectProdotti);
-
                 if (!$result) {
                     echo "Could not successfully run query ($sql) from DB: " . mysql_error();
                     exit();
@@ -28,26 +31,21 @@
                     echo "No rows found, nothing to print so am exiting";
                     exit();
                 }
-                echo "<table>
-                    <tr>
-                        <th>id prodotto</th>
-                        <th>Nome Prodotto</th>
-                        <th>Descrizione Prodotto</th>
-                        <th>Prezzo Prodotto</th>
-                        <th>Quantitá Prodotto</th>
-                    </tr>";
                 while($row = $result -> fetch_assoc()){
-                    echo "<tr>";
-                    echo "<td>".$row["id_prodotto"]."</td>";
-                    echo "<td>".$row["nome"]."</td>";
-                    echo "<td>".$row["descrizione"]."</td>";
-                    echo "<td>".$row["prezzo"]."€</td>";
-                    echo "<td>".$row["quantita"]."</td>";
-                    echo "</tr>";
+                    echo "<div class='card'>
+                        <div class='card-content'>
+                            <h3>".$row["nome"]."</h3>
+                            <h4>ID prodotto: ".$row["id_prodotto"]."</h4>
+                            <p>Descrizione:<br>".$row["descrizione"]."</p>
+                            <p>Prezzo:<br>".$row["prezzo"]."€</p>
+                            <p>Quantitá:<br>".$row["quantita"]." pezzi</p>
+                            <img src='img/".$row["image"]."'>
+                        </div>
+                    </div>";
                 }
-                echo "</table>";
             
             ?>
         </div>
-    </body>
+    </div>
+</body>
 </html>
--- initDB.bat
+++ initDB.bat
@@ -9,14 +9,16 @@ create table prodotto(
 	nome varchar(255),
 	descrizione varchar(255),
 	prezzo double(5,2),
-	quantita int
+	quantita int,
+	image varchar(255)
 );
 
-insert into prodotto (nome,descrizione,prezzo,quantita) values ("Coca Cola", "Bibita zuccherata energetica", 2.50, 40), 
-("Samson Galaxu 20-nota","Telefono di ultima generazione della famosa azienda samson",400, 20), 
-("Quaderno a quadri","Quaderno a quadri da 200 pagine",2,200),
-("Compaq","Computer potentissimo",150,26),
-("Astucccio Comics","Astuccio poco capiente",6.50,3000000),
-("Matite","TANTI COLORI",0.77,200);
+insert into prodotto (nome,descrizione,prezzo,quantita,image) values 
+("Coca Cola", "Bibita zuccherata energetica", 2.50, 40,"Coca Cola.png"), 
+("Samson Galaxu 20-nota","Telefono di ultima generazione della famosa azienda samson",400, 20,"Samson Galaxu 20-nota.png"), 
+("Quaderno a quadri","Quaderno a quadri da 200 pagine",2,200,"Quaderno a quadri.png"),
+("Compaq","Computer potentissimo",150,26,"Compaq.png"),
+("Astucccio Comics","Astuccio poco capiente",6.50,3000000,"Astucccio Comics.png"),
+("Matita","TANTI COLORI",0.77,200,"Matita.png");
 
 grant all on Esercitazione.* to php@localhost;
