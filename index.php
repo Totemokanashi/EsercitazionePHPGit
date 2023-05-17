e1805f0a88baf2be058c52fe7449bf201868e877
Totemokanashi <105808534+Totemokanashi@users.noreply.github.com>
2023-05-17 12:51:33 +0200

    aggiornato sito

--- index.php
+++ index.php
@@ -1,50 +1,53 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Esercitazione PHP Git</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body>
-    <?php
-            $mysqli = new mysqli("localhost","php","");
-            $mysqli -> select_db("prodotti");
+    <body style="background">
+        <div class="div">
+            <?php
+                $mysqli = new mysqli("localhost","php","");
+                $mysqli -> select_db("Esercitazione");
 
-            $selectProdotti = "SELECT * FROM prodotto";
+                $selectProdotti = "SELECT * FROM prodotto";
 
-            if ($mysqli -> connect_errno) {
-                echo "connesione fallita a mysql " . $mysqli -> connect_error;
-                exit();
-            }
+                if ($mysqli -> connect_errno) {
+                    echo "connesione fallita a mysql " . $mysqli -> connect_error;
+                    exit();
+                }
 
-            $result = $mysqli -> query($selectProdotti);
+                $result = $mysqli -> query($selectProdotti);
 
-            if (!$result) {
-                echo "Could not successfully run query ($sql) from DB: " . mysql_error();
-                exit();
-            }
+                if (!$result) {
+                    echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+                    exit();
+                }
+                
+                if ($result->num_rows == 0) {
+                    echo "No rows found, nothing to print so am exiting";
+                    exit();
+                }
+                echo "<table>
+                    <tr>
+                        <th>id prodotto</th>
+                        <th>Nome Prodotto</th>
+                        <th>Descrizione Prodotto</th>
+                        <th>Prezzo Prodotto</th>
+                        <th>Quantitá Prodotto</th>
+                    </tr>";
+                while($row = $result -> fetch_assoc()){
+                    echo "<tr>";
+                    echo "<td>".$row["id_prodotto"]."</td>";
+                    echo "<td>".$row["nome"]."</td>";
+                    echo "<td>".$row["descrizione"]."</td>";
+                    echo "<td>".$row["prezzo"]."€</td>";
+                    echo "<td>".$row["quantita"]."</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
             
-            if ($result->num_rows == 0) {
-                echo "No rows found, nothing to print so am exiting";
-                exit();
-            }
-            echo "<table>
-                <tr>
-                    <th>id prodotto</th>
-                    <th>Nome Prodotto</th>
-                    <th>Descrizione Prodotto</th>
-                    <th>Prezzo Prodotto</th>
-                    <th>Quantitá Prodotto</th>
-                </tr>";
-            while($row = $result -> fetch_assoc()){
-                echo "<tr>";
-                echo "<td>".$row["id_prodotto"]."</td>";
-                echo "<td>".$row["nome"]."</td>";
-                echo "<td>".$row["Descrizione"]."</td>";
-                echo "<td>".$row["Prezzo"]."$</td>";
-                echo "<td>".$row["quantita"]."</td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-              
-    ?>         
+            ?>
+        </div>
     </body>
 </html>
