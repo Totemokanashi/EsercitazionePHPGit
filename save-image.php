e08113fbe2f1e9a68327e94b653722fde5a5e202
Totemokanashi <105808534+Totemokanashi@users.noreply.github.com>
2023-05-20 16:45:11 +0200

    aggiunto save

---
+++ save-image.php
@@ -0,0 +1,39 @@
+<?php
+// Retrieve the raw request body
+$requestBody = file_get_contents('php://input');
+
+// Decode the JSON data
+$data = json_decode($requestBody, true);
+
+if ($data === null || !isset($data['filename']) || !isset($data['imageData'])) {
+  // Invalid request data
+  $response = array('success' => false, 'message' => 'Invalid request data.');
+  echo json_encode($response);
+  exit;
+}
+
+// Retrieve the data sent from the client-side
+$filename = $data['filename'];
+$imageData = $data['imageData'];
+
+// Remove the data URL prefix
+$parts = explode(',', $imageData);
+$imageData = base64_decode($parts[1]);
+
+// Define the destination directory to save the image
+$destinationDirectory = '';
+
+// Define the full path of the destination file
+$destinationPath = $destinationDirectory . $filename;
+
+// Save the image to the file system
+if (file_put_contents($destinationPath, $imageData)) {
+  // Image saved successfully
+  $response = array('success' => true, 'message' => 'Image saved successfully.');
+  echo json_encode($response);
+} else {
+  // Failed to save the image
+  $response = array('success' => false, 'message' => 'Failed to save the image.');
+  echo json_encode($response);
+}
+?>
